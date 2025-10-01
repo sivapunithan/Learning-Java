d804c2ecd3e567273644bc790dc7ac0199246e95
sivapunithan <punithansiva987@gmail.com>
2025-10-01 18:06:00 +0530

    Started 01-10-2015

---
+++ DayOne/LargestArray.java
@@ -0,0 +1,9 @@
+package DayOne;
+
+public class LargestArray {
+    public static void main (String[] args){
+        int arr [] = {12,45,87,56,56,87,99};
+        int largest = arr[0];
+        
+    }
+}
